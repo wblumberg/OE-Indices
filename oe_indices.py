cc9c9ef6d85831299b44d1a9b52f05d63c303045
Greg Blumberg <wblumberg@ou.edu>
2015-04-24 22:18:09 -0500

    BROKEN NOW: Began work on adding the capability to append to a file in prep for realtime running.

--- oe_indices.py
+++ oe_indices.py
@@ -6,32 +6,66 @@ import indices_helper as sti
 import sys
 from datetime import datetime
 import sharppy
-print sys.argv[1]
-#d = MFDataset(sys.argv[1])
 
+'''
+    oe_indices.py
 
-l = 2
-d = Dataset(sys.argv[1])
-height = d.variables['height'][:]
-pres = d.variables['pressure'][:l]
+    AUTHOR: Greg Blumberg (OU/CIMMS)
+
+    This script takes in OE-retrievals of thermodynamic profiles from
+    either AERIoe or MWRoe and performs a.) Monte Carlo sampling of the retrievals
+    to generate approximately 500 retrievals, and then b.) uses the SHARPpy
+    libraries to perform convection index calculations on all 500 retrievals.
+    After this the convection indices are sorted, the 25th, 50th, and 75th percentiles
+    are pulled out for each index and then saved in a netCDF file.
+
+    Running this script requires the netCDF4-python libraries, the SHARPpy libraries,
+    and IPython to perform the calculations in an efficient manner.
+
+    To run this script, use this command:
+        python oe_indices.py [-a or -w] <input_file>
+
+    where <input_file> is the path to the OE netCDF file and the flag -a means append to an existing file
+    and the -w means overwrite any existing file.
+
+    Before creating a new OE indices netCDF file, this program first checks for an existing file
+    because it may need to be appended to (for example if this script is running in real-time.
+
+'''
+flag = sys.argv[1]
+fn = sys.argv[2]
+name = fn.replace('aerioe1turn', 'aerioeidx1blum')
+name = name.replace('c1', 'c2')
+
+d = Dataset(fn)
 bt = d.variables['base_time'][:]
-to = d.variables['time_offset'][:l]
-Xop = d.variables['Xop'][:l]
-Sop = d.variables['Sop'][:l]
-converged_flag = d.variables['converged_flag'][:l]
+to = d.variables['time_offset'][:]
+
+existing_file = glob.glob(name)
+if len(existing_file) == 1 and flag == '-a':
+    temporary = Dataset(existing_file[0])
+    temp_bt = temporary.variables['base_time'][:] + temporary.variables['time_offset'][:]
+    beg_idx = len(to) - len(temp_bt)
+    end_idx = len(to)
+elif flag == '-w':
+    beg_idx = 0
+    end_idx = len(to)
+
+height = d.variables['height'][:]
+pres = d.variables['pressure'][beg_idx:end_idx]
+Xop = d.variables['Xop'][beg_idx:end_idx]
+Sop = d.variables['Sop'][beg_idx:end_idx]
+converged_flag = d.variables['converged_flag'][beg_idx:end_idx]
 height = height * 1000.
 
 num_perts = 700 # The ideal number of profiles to compute the index percentiles
 cush = 600 # The number of profiles where indices MUST be computed from (allows for crashing)
 ideb, details = sti.makeIndicesErrors(Xop, Sop, height, pres, num_perts, cush, converged_flag)
 
-fn = sys.argv[1]
-
-#spl2 = '.'.join(fn.split('/')[-1].split('.'))
-name = fn.replace('aerioe1turn', 'aerioeidx1blum')
-name = name.replace('c1', 'c2')
-#sp =  '/'.join(fn.split('/')[:-1])
-#std = sp + '/' + name  
+if flag == '-w' and len(glob.glob(name)) != 0:
+    os.system('rm ' + name)
+else:
+    print "Appending to the file."
 
 out = Dataset(name, 'w', format='NETCDF3_CLASSIC')
 
@@ -39,6 +73,7 @@ out.Date_created = datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
 out.description = "This file contains convective indices generated from thermodynamic profiles produced by an OE (i.e. AERIoe/MWRoe) algorithm.  The convective indicies within this file also contain estimates of uncertainity.  These were produced by Monte Carlo sampling the retrieved profile via its covariance matrix."
 out.comment = "Uses the SHARPpy libraries to calculate the convective indices."
 out.author = "Greg Blumberg; OU/CIMMS"
+out.email = 'wblumberg@ou.edu'
 out.input_file = fn
 out.num_perts = str(num_perts)
 out.cushon = str(cush)
@@ -76,27 +111,27 @@ for var_name in np.sort(details.keys()):
 	var[:] = data
 
 var = out.createVariable('hatchOpen', 'i4', ('time', ))
-var[:] = d.variables['hatchOpen'][:l]
+var[:] = d.variables['hatchOpen'][beg_idx:end_idx]
 var.long_name = d.variables['hatchOpen'].long_name
 var.units = str(d.variables['hatchOpen'].units)
 
 var = out.createVariable('converged_flag', 'i4', ('time', ))
-var[:] = d.variables['converged_flag'][:l]
+var[:] = d.variables['converged_flag'][beg_idx:end_idx]
 var.long_name = d.variables['converged_flag'].long_name
 var.units = str(d.variables['converged_flag'].units)
 
 var = out.createVariable('rms', 'f4', ('time', ))
-var[:] = d.variables['rms'][:l]
+var[:] = d.variables['rms'][beg_idx:end_idx]
 var.long_name = d.variables['rms'].long_name
 var.units = str(d.variables['rms'].units)
 
 var = out.createVariable('lwp', 'f4', ('time', ))
-var[:] = d.variables['lwp'][:l]
+var[:] = d.variables['lwp'][beg_idx:end_idx]
 var.long_name = d.variables['lwp'].long_name
 var.units = str(d.variables['lwp'].units)
 
 var = out.createVariable('qc_flag', 'i4', ('time', ))
-var[:] = d.variables['qc_flag'][:l]
+var[:] = d.variables['qc_flag'][beg_idx:end_idx]
 var.long_name = d.variables['qc_flag'].long_name
 var.units = str(d.variables['qc_flag'].units)
 
